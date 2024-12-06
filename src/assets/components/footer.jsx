7cf68ba32181f66189c9887b5338b817cc06f3c9
Alondra <alogadea@emeal.nttdata.com>
2024-12-06 15:49:34 -0300

    Refactor a TATETI game page

--- src/assets/components/footer.jsx
+++ src/assets/components/footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <footer>
       <div className="mapa-sitio">
-        <h6>Derechos reservados al espacio</h6>
+        <h6>Derechos reservados al espacio llamado MUNDO DE ALONDRA</h6>
       </div>
     </footer>
   );
